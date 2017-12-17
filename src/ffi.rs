7b8b79616e80a9917e688696db92b34d237254c9
rainer <rainer@pc>
2017-12-17 21:47:15 +0100

    * fixed constants for target armv7-unknown-linux-musleabihf and armv7-unknown-linux-gnueabihf (raspberry pi 2) * bumped libc to version 0.2.34

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,5 +1,13 @@
 use libc::{c_int, DIR, dirent};
 
+#[cfg(all(target_os="linux", not(target_arch ="arm")))]
+use libc;
+
+#[cfg(all(target_os="linux", not(target_arch ="arm")))]
+pub const O_DIRECTORY: c_int = libc::O_DIRECTORY;
+
+#[cfg(all(target_os="linux", target_arch ="arm"))]
+pub const O_DIRECTORY: c_int = 0o40000;
 
 pub const O_PATH: c_int = 0o10000000;
 
