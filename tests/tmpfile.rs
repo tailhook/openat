6ede55474bde5ac06d504c56f9fae87f35cb8c09
Paul Colomiets <paul@colomiets.name>
2018-08-07 00:53:57 +0300

    Add ls

--- tests/tmpfile.rs
+++ tests/tmpfile.rs
@@ -3,6 +3,7 @@ extern crate openat;
 
 use std::io::{self, Read, Write};
 use openat::Dir;
+use std::process::Command;
 
 #[test]
 #[cfg(target_os="linux")]
@@ -12,6 +13,7 @@ fn unnamed_tmp_file_link() -> Result<(), io::Error> {
     let mut f = dir.new_unnamed_file(0o777).expect("new file");
     f.write(b"hello\n").expect("write");
     dir.link_file_at(&f, "hello.txt").expect("linkat");
+    Command::new("ls").arg("-la").arg(tmp.path()).status().expect("ls");
     let mut f = dir.open_file("hello.txt").expect("read");
     let mut buf = String::with_capacity(10);
     f.read_to_string(&mut buf).expect("read data");
