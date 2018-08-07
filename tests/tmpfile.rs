cde31305ab1ec411975c4319d5c2049deb85f93b
Paul Colomiets <paul@colomiets.name>
2018-08-07 23:42:44 +0300

    Fix test of tmpfile on ubuntu trusty (travis)

--- tests/tmpfile.rs
+++ tests/tmpfile.rs
@@ -2,6 +2,7 @@ extern crate tempfile;
 extern crate openat;
 
 use std::io::{self, Read, Write};
+use std::os::unix::fs::PermissionsExt;
 use openat::Dir;
 
 #[test]
@@ -11,6 +12,9 @@ fn unnamed_tmp_file_link() -> Result<(), io::Error> {
     let dir = Dir::open(tmp.path())?;
     let mut f = dir.new_unnamed_file(0o777)?;
     f.write(b"hello\n")?;
+    // In glibc <= 2.22 permissions aren't set when using O_TMPFILE
+    // This includes ubuntu trusty on travis CI
+    f.set_permissions(PermissionsExt::from_mode(0o644));
     dir.link_file_at(&f, "hello.txt")?;
     let mut f = dir.open_file("hello.txt")?;
     let mut buf = String::with_capacity(10);
